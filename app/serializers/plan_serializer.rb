f3c5e0fa4ac060080997569d59e6a6e25b5d0395
Nurina Junus <nurina.junus@gmail.com>
2022-05-22 21:42:52 -0600

    serializers

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -2,5 +2,6 @@ class PlanSerializer < ActiveModel::Serializer
   include JSONAPI::Serializer
   attributes :id, :date, :user_id, :hike_id
   belongs_to :user, serializer: UserSerializer
-  
+  belongs_to :hike, serializer: HikeSerializer
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
+  include JSONAPI::Serializer
+
   attributes :id, :username, :name, :email, :password_digest
+  has_many :plans
 end
