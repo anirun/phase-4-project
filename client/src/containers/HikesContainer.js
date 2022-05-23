3a7f13b74b869d7ef8a752deb1d6b7e543396be9
Nurina Junus <nurina.junus@gmail.com>
2022-05-22 20:31:16 -0600

    Plans + Hikes Containers

--- client/src/containers/HikesContainer.js
+++ client/src/containers/HikesContainer.js
@@ -1,7 +1,23 @@
+import {useState, useEffect} from 'react';
+import HikesList from "../components/HikesList";
+
 const HikesContainer = () => {
+
+    const [hikes, setHikes] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/v1/hikes")
+        .then(r => r.json())
+        .then(data => setHikes(data.data.map(p => p.attributes)))
+        .catch(error => alert(error))
+    }, []);
+
     return (
-        <div>HikesContainer</div>
+        <div>
+            <h2>Hikes</h2>
+            <HikesList hikes={hikes} />
+        </div>
     )
 }
 
-export default HikesContainer
+export default HikesContainer;
---
+++ client/src/containers/PlansContainer.js
@@ -0,0 +1,23 @@
+import {useState, useEffect} from 'react';
+import PlansList from "../components/PlansList";
+
+const PlansContainer = () => {
+
+    const [plans, setPlans] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/v1/plans")
+        .then(r => r.json())
+        .then(data => setPlans(data.data.map(p => p.attributes)))
+        .catch(error => alert(error))
+    }, []);
+
+    return (
+        <div>
+            <h2>Plans</h2>
+            <PlansList plans={plans} />
+        </div>
+    )
+}
+
+export default PlansContainer;
